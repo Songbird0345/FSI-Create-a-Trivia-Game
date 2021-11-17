c3051918d99c01db0f1787df7b8435e3f0667f42
Amanda Strong <amanda@theheebners.com>
2021-11-17 07:50:22 -0800

    Changes Made

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your Code Here
+let userName = window.prompt('Please enter your Name:') 
+let userScore = 0;
+console.log('userName') 
+
+for(let i = 0; i < questions.length; i++){
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+    }
+}
+
+window.alert('Your Score is: '+userScore)
+
+let serializedGames = localStorage.getItem('games')
+let games;
+if(serializedGames === null){
+    games = []
+} else {
+    games = JSON.parse(serializedGames)
+}
+
+let game = {
+    user: userName,
+    score: userScore
+}
+games.push(game)
+console.log(games)
+
+serializedGames = JSON.stringify(games)
+localStorage.setItem('games', serializedGames)
